40f3c1fa017c908506eb7b5765143759ee5784d6
Matt . <matt.@Matts-MacBook-Pro.local>
2026-07-26 21:31:44 +0300

    Start app for e2e tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,15 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
+
   use: {
     baseURL: 'http://localhost:3000',
+  },
+
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:3000',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120_000,
   },
 });
